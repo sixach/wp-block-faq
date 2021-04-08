02f01086147ace19e5dd965ff24a90a728ed4dfe
Sajedeh Gooklani <gookalani@mail.com>
2021-04-08 21:38:53 +0300

    Uninstalled PostCSS Import and Position plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,11 @@
 module.exports = {
 	plugins: {
-		'postcss-import': {},
 		'postcss-selector-replace': {
 			before: [ '[prefix]' ],
 			after: [ 'sixa' ],
 		},
 		'postcss-nested-ancestors': {},
 		'postcss-nested': {},
-		'postcss-position': {},
 		autoprefixer: { grid: true },
 	},
 };

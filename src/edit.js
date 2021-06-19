1899214576261b9d0c4a2c88fda972fa8bee5c16
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-06-20 00:24:40 +0300

    Replaced `getEntityRecords` with `apiFetch` for both post-type and taxonomy

--- src/edit.js
+++ src/edit.js
@@ -1,17 +1,29 @@
 /**
  * Utility for libraries from the `Lodash`.
  */
-import { get, set, isArray, pickBy, identity } from 'lodash';
+import { get, set, isArray, slice, assign, map, find, pick } from 'lodash';
+
+/**
+ * Helper React components specific for Sixa projects.
+ */
+import { Loading } from '@sixach/wp-block-components';
 
 /**
  * Utility helper methods specific for Sixa projects.
  */
-import { LoadingSpinner, isNonEmptyArray } from '@sixa/wp-block-utils';
+import { isNonEmptyArray, blockClassName, selectOptions } from '@sixach/wp-block-utils';
+
+/**
+ * Retrieves the translation of text.
+ *
+ * @see     https://developer.wordpress.org/block-editor/packages/packages-i18n/
+ */
+import { __ } from '@wordpress/i18n';
 
 /**
  * Utility to make WordPress REST API requests. It's a wrapper around `window.fetch`.
  *
- * @see https://github.com/WordPress/gutenberg/blob/trunk/packages/api-fetch/README.md
+ * @see     https://github.com/WordPress/gutenberg/blob/trunk/packages/api-fetch/README.md
  */
 import apiFetch from '@wordpress/api-fetch';
 
@@ -19,37 +31,38 @@ import apiFetch from '@wordpress/api-fetch';
  * This packages includes a library of generic WordPress components to be used for
  * creating common UI elements shared between screens and features of the WordPress dashboard.
  *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-components/
+ * @see     https://developer.wordpress.org/block-editor/reference-guides/packages/packages-components/
  */
 import { addQueryArgs } from '@wordpress/url';
 
 /**
- * Retrieves the translation of text.
+ * WordPress specific abstraction layer atop React.
  *
- * @see https://developer.wordpress.org/block-editor/packages/packages-i18n/
+ * @see     https://github.com/WordPress/gutenberg/tree/HEAD/packages/element/README.md
  */
-import { __ } from '@wordpress/i18n';
+import { useState, useEffect, useRef, useMemo } from '@wordpress/element';
 
 /**
  * Data module to manage application state for both plugins and WordPress itself.
  * The data module is built upon and shares many of the same core principles of Redux.
  *
- * @see https://github.com/WordPress/gutenberg/tree/HEAD/packages/data/README.md
+ * @see     https://github.com/WordPress/gutenberg/tree/HEAD/packages/data/README.md
  */
-import { useSelect } from '@wordpress/data';
+import { useDispatch } from '@wordpress/data';
 
 /**
- * WordPress specific abstraction layer atop React.
+ * The compose package is a collection of handy Hooks and Higher Order Components (HOCs).
+ * The compose function is an alias to `flowRight` from Lodash.
  *
- * @see https://github.com/WordPress/gutenberg/tree/HEAD/packages/element/README.md
+ * @see  	https://github.com/WordPress/gutenberg/blob/trunk/packages/compose/README.md
  */
-import { useState, useEffect, useRef } from '@wordpress/element';
+import { compose, withInstanceId, withState } from '@wordpress/compose';
 
 /**
  * EventManager for JavaScript.
  * Hooks are used to manage component state and lifecycle.
  *
- * @see https://github.com/WordPress/gutenberg/blob/trunk/packages/hooks/README.md
+ * @see  	https://github.com/WordPress/gutenberg/blob/trunk/packages/hooks/README.md
  */
 import { applyFilters } from '@wordpress/hooks';
 
@@ -57,7 +70,7 @@ import { applyFilters } from '@wordpress/hooks';
  * React hook that is used to mark the block wrapper element.
  * It provides all the necessary props like the class name.
  *
- * @see https://developer.wordpress.org/block-editor/packages/packages-block-editor/#useBlockProps
+ * @see  	https://developer.wordpress.org/block-editor/packages/packages-block-editor/#useBlockProps
  */
 import { useBlockProps } from '@wordpress/block-editor';
 
@@ -65,14 +78,9 @@ import { useBlockProps } from '@wordpress/block-editor';
  * This packages includes a library of generic WordPress components to be used for
  * creating common UI elements shared between screens and features of the WordPress dashboard.
  *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-components/
+ * @see  	https://developer.wordpress.org/block-editor/reference-guides/packages/packages-components/
  */
-import { Notice, Placeholder, Dashicon } from '@wordpress/components';
-
-/**
- * Inspector Controls sidebar settings.
- */
-import Inspector from './inspector';
+import { Notice } from '@wordpress/components';
 
 /**
  * The loop template component.
@@ -80,86 +88,97 @@ import Inspector from './inspector';
 import Loop from './loop';
 
 /**
+ * Inspector Controls sidebar settings.
+ */
+import Inspector from './inspector';
+
+/**
+ * Scoped styles.
+ */
+import styles from './editor.module.css';
+
+/**
  * The edit function describes the structure of your block in the context of the
  * editor. This represents what the editor will render when the block is used.
  *
- * @see 	https://developer.wordpress.org/block-editor/developers/block-api/block-edit-save/#edit
- * @param 	{Object}  props 	Block meta-data properties.
- * @return 	{WPElement} 		Element to render.
+ * @see  	https://developer.wordpress.org/block-editor/developers/block-api/block-edit-save/#edit
+ * @param   {Object}    props 	    	Block meta-data properties.
+ * @return 	{WPElement}             	Element to render.
  */
-export default function Edit( props ) {
-	const { isSelected, attributes } = props;
-	const { order, orderBy, category, number } = attributes;
+function Edit( props ) {
 	const isStillMounted = useRef();
-	const [ categoriesList, setCategoriesList ] = useState( [] );
+	const { isSelected, attributes, instanceId, setState } = props;
+	const { ids, limit, category, order, orderby } = attributes;
+	const { createErrorNotice } = useDispatch( 'core/notices' );
+	const [ wpQuery, setWpQuery ] = useState( '' );
+	const blockProps = useBlockProps();
+	const className = blockClassName( get( blockProps, 'className' ) );
 	const postType = applyFilters( 'sixa.faqPostType', 'faq-item' );
-	const categoryEndpoint = applyFilters( 'sixa.faqCategoryEndpoint', 'faq-item-category' );
-	const categoryQueryArgs = applyFilters( 'sixa.faqCategoryQueryArgs', { per_page: -1 } );
-	const { getPosts, havePosts } = useSelect(
-		( select ) => {
-			const { getEntityRecords } = select( 'core' );
-			const wpQuery = getEntityRecords(
-				'postType',
-				postType,
-				pickBy(
-					{
-						order,
-						orderby: orderBy,
-						[ categoryEndpoint ]: category,
-						per_page: number,
-					},
-					( value ) => identity( value )
-				)
-			);
-
-			return {
-				havePosts: isNonEmptyArray( wpQuery ),
-				getPosts: wpQuery,
-			};
-		},
-		[ order, orderBy, category, number ]
+	const taxonomy = applyFilters( 'sixa.faqTaxonomy', 'faq-item-category' );
+	const noticeProps = { isDismissible: true, type: 'snackbar' };
+	const { havePosts, maxLimit, slicedQuery } = useMemo(
+		() => ( {
+			havePosts: isNonEmptyArray( wpQuery ),
+			maxLimit: get( wpQuery, 'length' ),
+			slicedQuery: isNonEmptyArray( ids ) ? map( ids, ( id ) => find( wpQuery, [ 'id', id ] ) ) : slice( wpQuery, 0, limit ),
+		} ),
+		[ wpQuery, limit, ids ]
 	);
+
+	useEffect( () => {
+		apiFetch( {
+			path: addQueryArgs( `/wp/v2/${ taxonomy }`, { per_page: -1 } ),
+		} )
+			.then( ( data ) => {
+				setState( ( state ) => assign( {}, state, { termOptions: map( data, ( term ) => pick( term, [ 'id', 'name', 'parent' ] ) ) } ) );
+			} )
+			.catch( () => {
+				createErrorNotice( __( 'Could not load faq categories.', 'sixa' ), noticeProps );
+			} );
+	}, [] );
 
 	useEffect( () => {
 		set( isStillMounted, 'current', true );
 
 		apiFetch( {
-			path: addQueryArgs( `/wp/v2/${ categoryEndpoint }`, categoryQueryArgs ),
+			path: addQueryArgs( `/wp/v2/${ postType }`, { per_page: -1, [ taxonomy ]: category || ' ', order, orderby } ),
 		} )
 			.then( ( data ) => {
 				if ( isStillMounted.current ) {
-					setCategoriesList( data );
+					setWpQuery( data );
+					setState( ( state ) => assign( {}, state, { postOptions: selectOptions( data, { id: 'value', 'title.rendered': 'label' }, [] ) } ) );
 				}
 			} )
 			.catch( () => {
 				if ( isStillMounted.current ) {
-					setCategoriesList( [] );
+					setWpQuery( [] );
+					createErrorNotice( __( 'Could not load faq posts.', 'sixa' ), noticeProps );
 				}
 			} );
 
 		return () => set( isStillMounted, 'current', false );
-	}, [] );
+	}, [ category, order, orderby ] );
 
 	return (
-		<>
-			<div { ...useBlockProps() }>
-				{ ! isArray( getPosts ) || ! get( isStillMounted, 'current' ) ? (
-					<Placeholder icon={ <Dashicon icon="admin-post" /> } label={ __( 'FAQ items', 'sixa' ) }>
-						<LoadingSpinner label={ ' ' } />
-					</Placeholder>
-				) : (
-					<>
-						{ havePosts ? (
-							<Loop getPosts={ getPosts } />
-						) : (
-							<Notice status="warning" isDismissible={ false }>
-								{ __( 'No posts found to display.', 'sixa' ) }
-							</Notice>
-						) }
-					</>
-				) }
-			</div>
-			{ isSelected && <Inspector { ...props } categoriesList={ categoriesList } /> }
-		</>
+		<div { ...blockProps }>
+			{ ! isArray( wpQuery ) || ! get( isStillMounted, 'current' ) ? (
+				<Loading label={ __( 'Fetching…', 'sixa' ) } />
+			) : havePosts ? (
+				<Loop className={ className } instanceId={ instanceId } query={ slicedQuery } styles={ styles } />
+			) : (
+				<Notice status="warning" className={ get( styles, 'notice' ) } isDismissible={ false }>
+					{ __( 'No F.A.Q posts found to display.', 'sixa' ) }
+				</Notice>
+			) }
+			{ isSelected && <Inspector { ...props } maxLimit={ maxLimit } /> }
+		</div>
 	);
 }
+
+export default compose( [
+	withInstanceId,
+	withState( {
+		postOptions: [],
+		termOptions: [],
+	} ),
+] )( Edit );

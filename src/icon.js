bfc8aaee630704a32ceced161b4eaf7d6723425c
Markus <markus.goeckeritz@sixa.ch>
2021-09-15 14:33:03 +0200

    moved icon to a separate file

---
+++ src/icon.js
@@ -0,0 +1,11 @@
+/**
+ * Sixa icon library.
+ */
+import { faq } from '@sixach/icon-library';
+
+/**
+ * Helper React components specific for Sixa projects.
+ */
+import { GenerateSvgPaths } from '@sixach/wp-block-components';
+
+export default <GenerateSvgPaths paths={ faq.paths } withSvgWrapper />;

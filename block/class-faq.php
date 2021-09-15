dfad297e8c2bc32b5b1c7ebb7729cc6ce6a4c561
Markus <markus.goeckeritz@sixa.ch>
2021-09-15 23:49:15 +0200

    added ABSPATH and class_exists checks

--- block/class-faq.php
+++ block/class-faq.php
@@ -16,86 +16,95 @@ namespace Sixa_Blocks;
 use Sixa_Blocks\FAQ\Includes\Loop;
 use Sixa_Snippets\Dashboard\Post_Type as Post_Type;
 
-/**
- * Block Class FAQ
- */
-final class FAQ extends Block {
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( FAQ::class ) ) :
 
 	/**
-	 * Registers the block using the metadata loaded from the `block.json` file.
-	 * Behind the scenes, it registers also all assets so they can be enqueued
-	 * through the block editor in the corresponding context.
-	 *
-	 * @see       https://developer.wordpress.org/block-editor/tutorials/block-tutorial/writing-your-first-block-type/
-	 * @since     1.0.0
-	 * @return    void
+	 * Block Class FAQ
 	 */
-	public static function register(): void {
-		self::register_post_type();
-		register_block_type_from_metadata(
-			dirname( __DIR__ ),
-			array(
-				'render_callback' => array( __CLASS__, 'render' ),
-			)
-		);
-	}
+	final class FAQ extends Block {
 
-	/**
-	 * Renders the `sixa/faq` block on the server.
-	 *
-	 * @since     1.0.0
-	 * @param     array $attributes    The block attributes.
-	 * @return    string
-	 */
-	public static function render( array $attributes = array() ): string {
-		$wrapper_attributes = get_block_wrapper_attributes();
-		$class_name         = sanitize_html_class( apply_filters( 'sixa_faq_block_class_name', 'wp-block-sixa-faq' ) );
-		$return             = sprintf( '<div %s>', $wrapper_attributes );
-		$attributes         = apply_filters( 'sixa_faq_block_attributes', $attributes );
-		$loop               = new Loop( $attributes, $class_name );
-		$return            .= apply_filters( 'sixa_faq_block_content', $loop->get_content(), $attributes, $class_name );
-		$return            .= '</div>';
+		/**
+		 * Registers the block using the metadata loaded from the `block.json` file.
+		 * Behind the scenes, it registers also all assets so they can be enqueued
+		 * through the block editor in the corresponding context.
+		 *
+		 * @see       https://developer.wordpress.org/block-editor/tutorials/block-tutorial/writing-your-first-block-type/
+		 * @since     1.0.0
+		 * @return    void
+		 */
+		public static function register(): void {
+			self::register_post_type();
+			register_block_type_from_metadata(
+				dirname( __DIR__ ),
+				array(
+					'render_callback' => array( __CLASS__, 'render' ),
+				)
+			);
+		}
 
-		return apply_filters( 'sixa_faq_block_render_html', $return );
-	}
+		/**
+		 * Renders the `sixa/faq` block on the server.
+		 *
+		 * @since     1.0.0
+		 * @param     array $attributes    The block attributes.
+		 * @return    string
+		 */
+		public static function render( array $attributes = array() ): string {
+			$wrapper_attributes = get_block_wrapper_attributes();
+			$class_name         = sanitize_html_class( apply_filters( 'sixa_faq_block_class_name', 'wp-block-sixa-faq' ) );
+			$return             = sprintf( '<div %s>', $wrapper_attributes );
+			$attributes         = apply_filters( 'sixa_faq_block_attributes', $attributes );
+			$loop               = new Loop( $attributes, $class_name );
+			$return            .= apply_filters( 'sixa_faq_block_content', $loop->get_content(), $attributes, $class_name );
+			$return            .= '</div>';
 
-	/**
-	 * Registers the custom post-type required for this block.
-	 *
-	 * @see       https://sixach.github.io/sixa-wp-snippets/#/dashboard/post-type
-	 * @since     1.0.0
-	 * @return    void
-	 */
-	public static function register_post_type(): void {
-		new Post_Type(
-			array(
-				apply_filters(
-					'sixa_faq_block_register_post_type_args',
-					array(
-						'key'           => apply_filters( 'sixa_faq_block_post_type', 'faq-item' ),
-						'singular_name' => __( 'FAQ Item', 'sixa' ),
-						'plural_name'   => __( 'FAQ Items', 'sixa' ),
-						'args'          => apply_filters(
-							'sixa_faq_block_post_type_args',
-							array(
-								'publicly_queryable' => false,
-								'menu_icon'          => 'dashicons-editor-help',
-							)
-						),
-						'taxonomies'    => array(
-							array(
-								'key'  => apply_filters( 'sixa_faq_block_taxonomy', 'faq-item-category' ),
-								'args' => apply_filters(
-									'sixa_faq_block_taxonomy_args',
-									array(
-										'publicly_queryable' => false,
-									)
+			return apply_filters( 'sixa_faq_block_render_html', $return );
+		}
+
+		/**
+		 * Registers the custom post-type required for this block.
+		 *
+		 * @see       https://sixach.github.io/sixa-wp-snippets/#/dashboard/post-type
+		 * @since     1.0.0
+		 * @return    void
+		 */
+		public static function register_post_type(): void {
+			new Post_Type(
+				array(
+					apply_filters(
+						'sixa_faq_block_register_post_type_args',
+						array(
+							'key'           => apply_filters( 'sixa_faq_block_post_type', 'faq-item' ),
+							'singular_name' => __( 'FAQ Item', 'sixa' ),
+							'plural_name'   => __( 'FAQ Items', 'sixa' ),
+							'args'          => apply_filters(
+								'sixa_faq_block_post_type_args',
+								array(
+									'publicly_queryable' => false,
+									'menu_icon'          => 'dashicons-editor-help',
+								)
+							),
+							'taxonomies'    => array(
+								array(
+									'key'  => apply_filters( 'sixa_faq_block_taxonomy', 'faq-item-category' ),
+									'args' => apply_filters(
+										'sixa_faq_block_taxonomy_args',
+										array(
+											'publicly_queryable' => false,
+										)
+									),
 								),
 							),
-						),
-					)
-				),
-			)
-		);
+						)
+					),
+				)
+			);
+		}
 	}
-}
+
+endif;

f0a3d970f11b2bba703d57f13a9afc39aa6cfa87
Markus <markus.goeckeritz@sixa.ch>
2022-01-08 18:09:17 +0100

    replaced sanitize functions with esc_ functions

--- block/templates/content-faq-item.php
+++ block/templates/content-faq-item.php
@@ -19,11 +19,11 @@ $faq_post   = $args['faq_post'] ?? '';
 $class_name = $args['class_name'] ?? '';
 
 ?>
-<details class="<?php echo sanitize_html_class( $class_name ); ?>__item">
-	<summary class="<?php echo sanitize_html_class( $class_name ); ?>__title">
+<details class="<?php echo esc_attr( $class_name ); ?>__item">
+	<summary class="<?php echo esc_attr( $class_name ); ?>__title">
 		<?php echo esc_html( get_the_title( $faq_post ) ); ?>
 	</summary>
-	<div class="<?php echo sanitize_html_class( $class_name ); ?>__content">
+	<div class="<?php echo esc_attr( $class_name ); ?>__content">
 		<?php echo apply_filters( 'the_content', $faq_post->post_content ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 	</div>
 </details>

e3088e610817cf42ea7f04789a3c160531c36c1f
r007 <moninsergei@gmail.com>
2022-01-09 16:59:52 +0400

    Update plugin meta data to match readme.txt, merge readme.txt content into README.md

--- index.php
+++ index.php
@@ -4,15 +4,20 @@
  *
  * @wordpress-plugin
  * Plugin Name:          Sixa - FAQ Block
- * Description:          FAQ block for WordPress editor.
+ * Description:          Display Frequently Asked Questions (FAQs) in an accordion on your website. Add, store, and maintain your FAQs in a custom post type and create categories to group your FAQs. Order your FAQs alphabetically or by their creation date and automatically include newly created FAQ items in the block output. Filter the FAQ posts that should be displayed by category. You may also set FAQ items to display and the order in which they should be displayed manually.
+ * Tags:                 faq, questions, answers, accordion, block, gutenberg, sixa
+ * Contributors:         sixa, mahdiyazdani, gookaani, kuserich
  * Version:              1.0.0
+ * Stable tag:           1.0.0
  * Requires at least:    5.7
+ * Tested up to:         5.8
  * Requires PHP:         7.3
  * Author:               sixa AG
  * Author URI:           https://sixa.ch
  * License:              GPL v3 or later
  * License URI:          https://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain:          sixa-block-faq
+ * Donate link:          https://sixa.com/
  *
  * @package              Sixa_Blocks
  */

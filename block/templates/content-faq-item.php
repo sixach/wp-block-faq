36c9e33178750ef9aee66f8f9bacf9432a35a4fb
Adrian Firoiu <mihai.a.firoiu@gmail.com>
2022-01-19 16:31:15 +0100

    Wrap title content to align content in Safari and remove browser markers

--- block/templates/content-faq-item.php
+++ block/templates/content-faq-item.php
@@ -21,7 +21,9 @@ $class_name = $args['class_name'] ?? '';
 ?>
 <details class="<?php echo esc_attr( $class_name ); ?>__item">
 	<summary class="<?php echo esc_attr( $class_name ); ?>__title">
-		<?php echo esc_html( get_the_title( $faq_post ) ); ?>
+		<span class="<?php echo esc_attr( $class_name ); ?>__container">
+			<?php echo esc_html( get_the_title( $faq_post ) ); ?>
+		</span>
 	</summary>
 	<div class="<?php echo esc_attr( $class_name ); ?>__content">
 		<?php echo apply_filters( 'the_content', $faq_post->post_content ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
